1460a6f49e6d2c6098405bab6aa6dafbffa0e3b6
KhueHoangMinh <hoangkhue1803@gmail.com>
2023-06-20 08:54:26 -0700

    update

--- Program.cs
+++ Program.cs
@@ -14,128 +14,6 @@ namespace Cyberpunk77022
     public class Program
     {
 
-        public static void StartServer()
-        {
-            // Get Host IP Address that is used to establish a connection
-            // In this case, we get one IP address of localhost that is IP : 127.0.0.1
-            // If a host has multiple addresses, you will get a list of addresses
-            IPHostEntry host = Dns.GetHostEntry("localhost");
-            IPAddress ipAddress = host.AddressList[0];
-            IPEndPoint localEndPoint = new IPEndPoint(ipAddress, 11000);
-
-            try
-            {
-
-                // Create a Socket that will use Tcp protocol
-                Socket listener = new Socket(ipAddress.AddressFamily, SocketType.Stream, ProtocolType.Tcp);
-                // A Socket must be associated with an endpoint using the Bind method
-                listener.Bind(localEndPoint);
-                // Specify how many requests a Socket can listen before it gives Server busy response.
-                // We will listen 10 requests at a time
-                listener.Listen(10);
-
-                Console.WriteLine("Waiting for a connection...");
-                Socket handler = listener.Accept();
-
-                // Incoming data from the client.
-                string data = null;
-                byte[] bytes = null;
-
-                while (true)
-                {
-                    bytes = new byte[1024];
-                    int bytesRec = handler.Receive(bytes);
-                    data += Encoding.ASCII.GetString(bytes, 0, bytesRec);
-                    if (data.IndexOf("<EOF>") > -1)
-                    {
-                        break;
-                    }
-                }
-
-                Console.WriteLine("Text received : {0}", data);
-
-                byte[] msg = Encoding.ASCII.GetBytes(data);
-                handler.Send(msg);
-                handler.Shutdown(SocketShutdown.Both);
-                handler.Close();
-            }
-            catch (Exception e)
-            {
-                Console.WriteLine(e.ToString());
-            }
-
-            Console.WriteLine("\n Press any key to continue...");
-            Console.ReadKey();
-        }
-
-        // Client app is the one sending messages to a Server/listener.
-        // Both listener and client can send messages back and forth once a
-        // communication is established.
-
-        public static void StartClient()
-        {
-            byte[] bytes = new byte[1024];
-
-            try
-            {
-                // Connect to a Remote server
-                // Get Host IP Address that is used to establish a connection
-                // In this case, we get one IP address of localhost that is IP : 127.0.0.1
-                // If a host has multiple addresses, you will get a list of addresses
-                IPHostEntry host = Dns.GetHostEntry("localhost");
-                IPAddress ipAddress = host.AddressList[0];
-                IPEndPoint remoteEP = new IPEndPoint(ipAddress, 11000);
-
-                // Create a TCP/IP  socket.
-                Socket sender = new Socket(ipAddress.AddressFamily,
-                    SocketType.Stream, ProtocolType.Tcp);
-
-                // Connect the socket to the remote endpoint. Catch any errors.
-                try
-                {
-                    // Connect to Remote EndPoint
-                    sender.Connect(remoteEP);
-
-                    Console.WriteLine("Socket connected to {0}",
-                        sender.RemoteEndPoint.ToString());
-
-                    // Encode the data string into a byte array.
-                    byte[] msg = Encoding.ASCII.GetBytes("This is a test<EOF>");
-
-                    // Send the data through the socket.
-                    int bytesSent = sender.Send(msg);
-
-                    // Receive the response from the remote device.
-                    int bytesRec = sender.Receive(bytes);
-                    Console.WriteLine("Echoed test = {0}",
-                        Encoding.ASCII.GetString(bytes, 0, bytesRec));
-
-                    // Release the socket.
-                    sender.Shutdown(SocketShutdown.Both);
-                    sender.Close();
-
-                }
-                catch (ArgumentNullException ane)
-                {
-                    Console.WriteLine("ArgumentNullException : {0}", ane.ToString());
-                }
-                catch (SocketException se)
-                {
-                    Console.WriteLine("SocketException : {0}", se.ToString());
-                }
-                catch (Exception e)
-                {
-                    Console.WriteLine("Unexpected exception : {0}", e.ToString());
-                }
-
-            }
-            catch (Exception e)
-            {
-                Console.WriteLine(e.ToString());
-            }
-        }
-
-
         static void Main()
         {
             int WIDTH = 1920;
@@ -150,19 +28,7 @@ namespace Cyberpunk77022
 
             string _textInput = SplashKit.TextInput(window);
 
-            //SplashKit.CurrentWindowToggleFullscreen();
-
-            // Get device ip address
-
-            var host = Dns.GetHostEntry(Dns.GetHostName());
-            foreach (var ip in host.AddressList)
-            {
-                if (ip.AddressFamily == AddressFamily.InterNetwork)
-                {
-                    Console.WriteLine(ip.ToString());
-                }
-            }
-
+            SplashKit.CurrentWindowToggleFullscreen();
 
             // Game loop
 
@@ -170,19 +36,7 @@ namespace Cyberpunk77022
             {
                 CURRENT_UPDATE_TICK = DateTime.UtcNow.Ticks;
 
-
-
-
                 manager.Update();
-
-                //if(SplashKit.MouseClicked(MouseButton.LeftButton))
-                //{
-                //    SplashKit.StartReadingText(new Rectangle() { X = 100, Y = 100, Width = 100, Height = 100 });
-                //}
-                //if(SplashKit.ReadingText())
-                //{
-                //    _textInput = SplashKit.TextInput(window);
-                //}
 
                 SplashKit.ProcessEvents();
                 if (DateTime.UtcNow.Ticks - CURRENT_FRAME_TICK >= TIME_BETWEEN_FRAMES)
@@ -190,13 +44,11 @@ namespace Cyberpunk77022
                     CURRENT_FRAME_TICK = DateTime.UtcNow.Ticks;
                     SplashKit.ClearScreen(Color.Black);
                     manager.Draw();
-                    //SplashKit.DrawText(_textInput, Color.White, "font", 50, 100, 100);
                     SplashKit.RefreshScreen();
                 }
                 SplashKit.Delay((uint)TIME_BETWEEN_UPDATES / 10000);
             } while (
-                false 
-                //!SplashKit.WindowCloseRequested(window) && !SplashKit.KeyDown(KeyCode.EscapeKey)
+                !SplashKit.WindowCloseRequested(window) && !SplashKit.KeyDown(KeyCode.EscapeKey)
             );
 
         }
