81858a7fe84ebed41ff8da5b186e21a0984a996d
KhueHoangMinh <hoangkhue1803@gmail.com>
2023-07-11 03:27:21 -0700

    test

--- Program.cs
+++ Program.cs
@@ -18,8 +18,8 @@ namespace Cyberpunk77022
         {
             int WIDTH = 1920;
             int HEIGHT = 1080;
-            long TIME_BETWEEN_UPDATES = 120000;
-            long TIME_BETWEEN_FRAMES = 0;
+            long TIME_BETWEEN_UPDATES = 150000;
+            long TIME_BETWEEN_FRAMES = 12000;
             long CURRENT_UPDATE_TICK = 0;
             long CURRENT_FRAME_TICK = 0;
 
@@ -28,37 +28,26 @@ namespace Cyberpunk77022
 
             string _textInput = SplashKit.TextInput(window);
 
-           
+
 
             // Game loop
 
-            do
-            {
-                if (DateTime.UtcNow.Ticks - CURRENT_UPDATE_TICK >= TIME_BETWEEN_UPDATES)
+                do
                 {
-                    CURRENT_UPDATE_TICK = DateTime.UtcNow.Ticks;
-                }
-
-                manager.Update();
-                SplashKit.ProcessEvents();
-
-                //if(SplashKit.MouseClicked(MouseButton.LeftButton))
-                //{
-                //    SplashKit.StartReadingText(new Rectangle() { X = 100, Y = 100, Width = 100, Height = 100 });
-                //}
-                //if(SplashKit.ReadingText())
-                //{
-                //    _textInput = SplashKit.TextInput(window);
-
-                if (DateTime.UtcNow.Ticks - CURRENT_FRAME_TICK >= TIME_BETWEEN_FRAMES)
-                {
-                    CURRENT_FRAME_TICK = DateTime.UtcNow.Ticks;
-                    SplashKit.ClearScreen(Color.Black);
-                    manager.Draw();
-                    //SplashKit.DrawText(_textInput, Color.White, "font", 50, 100, 100);
-                    SplashKit.RefreshScreen();
-                }
-                SplashKit.Delay((uint)TIME_BETWEEN_UPDATES / 10000);
+                    if (DateTime.UtcNow.Ticks - CURRENT_UPDATE_TICK >= TIME_BETWEEN_UPDATES)
+                    {
+                        CURRENT_UPDATE_TICK = DateTime.UtcNow.Ticks;
+                        SplashKit.ProcessEvents();
+                        manager.Update();
+                    }
+                    if (DateTime.UtcNow.Ticks - CURRENT_FRAME_TICK >= TIME_BETWEEN_FRAMES)
+                    {
+                        CURRENT_FRAME_TICK = DateTime.UtcNow.Ticks;
+                        SplashKit.ClearScreen(Color.Black);
+                        manager.Draw();
+                        //SplashKit.DrawText(_textInput, Color.White, "font", 50, 100, 100);
+                        SplashKit.RefreshScreen();
+                    }
             } while (
                 //false 
                 !SplashKit.WindowCloseRequested(window) && !SplashKit.KeyDown(KeyCode.EscapeKey)
